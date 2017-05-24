fbeb1e98bc454df64c49fe77f3985a67fa964217
Oleg Dutsyak <oleg.dystak@gmail.com>
2017-05-24 17:49:19 +0300

    Update RefreshScheduleMessage.cs

--- CinemaTickets/CinemaTickets.UI/Messages/RefreshScheduleMessage.cs
+++ CinemaTickets/CinemaTickets.UI/Messages/RefreshScheduleMessage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+// Review OD: There are unnecessary usings
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace CinemaTickets.UI.Messages
 {
     public class RefreshScheduleMessage
     {
+        // Review OD: Maybe there should be some members?
     }
 }
